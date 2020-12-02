1b06e1188b8027fecbbb775f52cfc25e3cb26848
PoshSquash <57773399+PoshSquash@users.noreply.github.com>
2020-12-02 15:14:43 -0800

    Tutorial 2.2.4 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,7 +26,14 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+       
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+            // Launch a projecticle from player
+        }
+
     }
 }
